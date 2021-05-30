575ab21bba3c58202304e639a765f3dfebd02d4f
iMelki <melki@post.bgu.ac.il>
2021-05-30 21:06:16 +0300

    started Angular app

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,8 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -55,6 +57,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(policy => {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+            });
 
             app.UseAuthorization();
 
